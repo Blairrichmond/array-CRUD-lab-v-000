f1006c138680104b4950f93db8c8cf97dfd1d23a
Brent  Ransom <sadumedusa@gmail.com>
2018-01-11 16:36:03 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -18,7 +18,8 @@ end
 
 def remove_element_from_end_of_array(array)
   array = ["I", "am", "really", "learning", "arrays!"]
-  array.pop("arrays!")
+  array_pop = array.pop
+  puts array_pop
 end
 
 def remove_element_from_start_of_array(array)
